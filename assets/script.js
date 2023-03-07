ddd8b90818fab1335e8ec046570b6e8b5b13b168
AJoelj84 <ajoelj84@gmail.com>
2023-03-06 20:44:44 -0500

    Added the JavaScript Code back in

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var getWeather = function(city, state) {
+    var openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +','+ state +' &appid=263899f28c1a4fdfb9c42daf32e3c285';
+    console.log('get current weather');
+    fetch(openWeatherUrl)
+        .then (function(response){
+            console.log('Got weather from api');
+            return response.json();
+        })
+        .then (function(data){
+            console.log('Weather', data);
+        })
+};
+
+
+
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '3f7720283amsh832fcd99762aa5ep13ac18jsn2a3f879a1146',
+// 		'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://trailapi-trailapi.p.rapidapi.com/trails/%7Bid%7D/maps/', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
