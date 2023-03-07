5bf197abd1f64261719d230b727f8c309f69572a
AJoelj84 <ajoelj84@gmail.com>
2023-03-06 19:44:46 -0500

    Base framework for JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
+var cityName
+var stateInitial
 
 
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+ lat + '&lon=' + lon +'&appid=263899f28c1a4fdfb9c42daf32e3c285',{
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+ cityName +','+ stateInitial +' &appid=263899f28c1a4fdfb9c42daf32e3c285',{
 })
 .then(function(response){
     return response.json();
@@ -9,7 +10,6 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+ lat + '&lon=' + l
 .then(function(data){
     console.log(data);
 });
-
 
 
 
@@ -34,16 +34,16 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+ lat + '&lon=' + l
 
 
 
-var trailApi = {
+const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '3f7720283amsh832fcd99762aa5ep13ac18jsn2a3f879a1146',
 		'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
 	}
 };
+
 fetch('https://trailapi-trailapi.p.rapidapi.com/trails/%7Bid%7D/maps/', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-    console.log(trailApi)
 
